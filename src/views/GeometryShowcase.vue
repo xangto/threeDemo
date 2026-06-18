a6dd50933e841b957dd6868be33ffe538571319b
wangtao <2281659624@qq.com>
2026-06-18 23:37:52 +0800

    feat(view): 新增常用几何体展示页视图

---
+++ src/views/GeometryShowcase.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted, useTemplateRef } from 'vue'
+import { useCommonGeometry } from '@/composables/useCommonGeometry'
+
+const container = useTemplateRef('container')
+onMounted(() => {
+  useCommonGeometry(<HTMLDivElement>container.value)
+})
+</script>
+
+<template>
+  <div
+    ref="container"
+    class="w-full h-full relative"
+  ></div>
+</template>
+
+<style scoped></style>
